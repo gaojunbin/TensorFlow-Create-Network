e7aee3f3d09d638080e551404b551c74ce8a9ab7
gaojunbin <913290959@qq.com>
2020-05-10 17:09:37 +0800

    modify ： 修改了部署在谷歌硬盘上的部分代码

--- Vgg_16.py
+++ Vgg_16.py
@@ -135,13 +135,18 @@ def backward(datasets, label, test_data, test_label):
     gloabl_steps = tf.Variable(0, trainable=False)  # 计数器，用来记录运行了几轮的BATCH_SIZE，初始为0，设置为不可训练
     learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, gloabl_steps, LEARNING_RATE_STEP, LEARNING_RATE_DECAY, staircase=True)
     keep_prob = tf.placeholder(tf.float32, name = "keep_prob")
-    y = VGG16(X,keep_prob=0.9)
+    y = VGG16(X,keep_prob)
     global_step = tf.Variable(0, trainable=False)
     loss_mse = tf.reduce_mean(-tf.reduce_sum(Y_ * tf.log(y), reduction_indices=[1]))
     # loss_mse = tf.reduce_mean(tf.square(y-Y_)) 
     # train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_mse)
     # train_step=tf.train.MomentumOptimizer(0.001,0.9).minimize(loss_mse) #其他方法
-    train_step=tf.train.AdamOptimizer(0.001).minimize(loss_mse)  
+    train_step=tf.train.AdamOptimizer(0.001).minimize(loss_mse)
+    #准确率
+    accuracy_y = VGG16(X,keep_prob)
+    accuracy = tf.equal(tf.argmax(accuracy_y, 1), tf.argmax(Y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(accuracy, tf.float32))
+
     saver = tf.train.Saver()
     with tf.Session() as sess:
         init_op = tf.global_variables_initializer()
@@ -152,18 +157,22 @@ def backward(datasets, label, test_data, test_label):
         for i in range(STEPS):
             start = (i*BATCH_SIZE) % 20
             end = start + BATCH_SIZE
-            sess.run(train_step, feed_dict={X: datasets[start:end], Y_: label[start:end]})
+            sess.run(train_step, feed_dict={X: datasets[start:end], Y_: label[start:end],keep_prob:0.9})
             if i % 10 == 0:
                 train_loss = sess.run(loss_mse, feed_dict={X: datasets, Y_: label, keep_prob:1})
-                total_loss = sess.run(loss_mse, feed_dict={X: test_data, Y_: test_label, keep_prob:1})
-                if total_loss < min_loss:
-                    min_loss = total_loss
+                val_loss = sess.run(loss_mse, feed_dict={X: test_data, Y_: test_label, keep_prob:1})
+                train_accuracy = sess.run(accuracy, feed_dict={X: datasets, Y_: label,keep_prob:1})
+                test_accuracy = sess.run(accuracy, feed_dict={X: test_data, Y_: test_label,keep_prob:1})
+                if val_loss < min_loss:
+                    min_loss = val_loss
                 f = open('./text/loss.txt', 'a')
-                f.write("After %d training step(s), loss_mse on train data is %g, loss_mse on val data is %g, min_loss is %g\n" % (i, train_loss, total_loss, min_loss))
-                print("After %d training step(s), loss_mse on train data is %g, loss_mse on val data is %g, min_loss is %g" % (i, train_loss, total_loss, min_loss))
+                f.write("训练步数：%d,loss(train data):%g,loss(test data):%g**accuracy(train data):%g,accuracy(test data):%g\n" %(i,train_loss,val_loss,train_accuracy,test_accuracy))
+                print("训练步数：%d,loss(train data):%g,loss(test data):%g**accuracy(train data):%g,accuracy(test data):%g\n" %(i,train_loss,val_loss,train_accuracy,test_accuracy))
+                # f.write("After %d training step(s), loss_mse on train data is %g, loss_mse on val data is %g, min_loss is %g\n" % (i, train_loss, val_loss, min_loss))
+                # print("After %d training step(s), loss_mse on train data is %g, loss_mse on val data is %g, min_loss is %g" % (i, train_loss, val_loss, min_loss))
                 f.close()
-            if i % 100 == 0:
-                saver.save(sess, './checkpoint/variable', global_step = i)
+            # if i % 100 == 0:
+            #     saver.save(sess, './checkpoint/variable', global_step = i)
  
 def main():
     # datasets, label, test_data, test_label = reload_all_data()
